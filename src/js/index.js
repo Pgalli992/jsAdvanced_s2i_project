4456ce2997a98c87868e0e47c038a1b5fb7f6150
Paolo <p.galli@hotmail.it>
2024-05-16 22:57:02 +0200

    Added console log

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import * as modal from "./modal.js";
+
+const getNews = async function () {
+  try {
+    const res = await fetch(
+      "https://hacker-news.firebaseio.com/v0/newstories.json"
+    );
+    const data = await res.json();
+    console.log(res, data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+const getNewsInfo = async function () {
+  try {
+    const res = await fetch(
+      "https://hacker-news.firebaseio.com/v0/item/40352258.json"
+    );
+    const data = await res.json();
+    console.log(data);
+    console.log("Hello world");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getNews();
+getNewsInfo();
+
+const loadNews = async function () {};
---
+++ src/js/modal.js
@@ -0,0 +1,9 @@
+const createNewsObj = function (data) {
+  return {
+    author: data.by,
+    id: data.id,
+    time: time.data,
+    title: data.title,
+    url: data.url,
+  };
+};
