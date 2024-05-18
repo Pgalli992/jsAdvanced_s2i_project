e1220e240156ccbeaad1b30e607fef9d2197f399
Paolo <p.galli@hotmail.it>
2024-05-18 22:54:12 +0200

    make calls AJAX work

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,62 @@
-import { createNewsObj } from "./modal.js";
+import { state, createObj, createNewsMarkup } from "./model.js";
 
+// Getting new sroties ID
 const getNews = async function () {
   try {
     const res = await fetch(
       "https://hacker-news.firebaseio.com/v0/newstories.json"
     );
     const data = await res.json();
-    console.log(res, data);
+
+    chooseNews(data);
+    chooseNews(data);
+    chooseNews(data);
+    chooseNews(data);
+    chooseNews(data);
+    renderNews(state.results);
   } catch (err) {
     console.error(err);
   }
 };
+
+const chooseNews = function (data) {
+  const start = state.results.length;
+  console.log(start);
+  const end = start + 10;
+
+  state.results.push(...data.slice(start, end));
+  console.log(state.results);
+};
+
+const renderNews = function (array) {
+  array.map(async (id) => {
+    const res = await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    );
+    const data = await res.json();
+    // Filling state object with data
+    state.info = createObj(data);
+    // Creating a markup to render the news
+    createNewsMarkup(state.info);
+  });
+};
+
+// Getting all information about news
 const getNewsInfo = async function (id) {
   try {
     const res = await fetch(
       `https://hacker-news.firebaseio.com/v0/item/${id}.json`
     );
     const data = await res.json();
-    createNewsObj(data);
-    console.log(data);
+    // Filling state object with data
+    state.info = createObj(data);
+    // Creating a markup to render the news
+    createNewsMarkup(state.info);
   } catch (err) {
     console.error(err);
   }
 };
 
-const renderNews = function (obj) {};
-
 getNews();
-getNewsInfo(40387195);
 
 const loadNews = function () {};
---
+++ src/js/model.js
@@ -0,0 +1,51 @@
+const resultSection = document.querySelector(".result__section");
+
+export const state = {
+  info: {},
+  page: 1,
+  results: [],
+  resultsPerPage: 10, // RES_PER_PAGE
+};
+
+// Converting time info (in MS) to a date and time string
+const timeConverter = function (timeInMS) {
+  let dateTime = new Date(timeInMS * 1000);
+  let date = dateTime.toLocaleDateString("it-IT");
+  let time = dateTime.toLocaleTimeString("it-IT").slice(0, -3);
+  return `[${date} - ${time}]`;
+};
+
+// Creatingg new objecy to store result information
+export const createObj = function (data) {
+  const info = data;
+  return {
+    author: info.by,
+    id: info.id,
+    time: timeConverter(info.time),
+    title: info.title,
+    url: info.url,
+  };
+};
+
+// Creating markup to render results
+export const createNewsMarkup = function (data) {
+  console.log(data.url);
+  const markup = `<div
+  class="flex flex-col items-center relative bg-gray-900 text-slate-50 py-4"
+>
+  <h1 class="text-xl font-bold">
+    ${data.title}
+  </h1>
+  <span class="my-2">${data.author}</span>
+  <span class="text-blue-400">${data.time}</span>
+  <button
+    class="w-max absolute text-gray-900 font-bold bg-slate-50 rounded-xl
+    shadow-[0_25px_50px_-12px_rgba(255,255,255,.4)] py-2 px-4 justify-self-center
+    duration-150 right-3 top-1/2 -translate-y-1/2 hover:-translate-y-6 hover:bg-blue-400 active:-translate-y-1/2"
+  >
+  ${!data.url ? `<a>No Link<a/>` : `<a href="${data.url}" target="_blank">Open</a>`}
+    
+  </button>
+</div>`;
+  resultSection.insertAdjacentHTML("beforeend", markup);
+};
